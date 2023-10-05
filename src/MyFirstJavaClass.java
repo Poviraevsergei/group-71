0ad8be78dacae2c30dd933723829e7e35f092b2b
Poviraevsergei <poviraevsergei@gmail.com>
2023-10-05 20:13:50 +0300

    Initial commit

---
+++ src/MyFirstJavaClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MyFirstJavaClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if (num == 100){
+            for (int i = num; i > 0; i--) {
+                if (num % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
